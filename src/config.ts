25d4c87f30bcb93ddb0d8207751b90eb38cb09ea
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-03-01 20:48:00 +0900

    feat: add preset

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,13 @@
 export interface Config {
-  ignorePattern?: string | RegExp
+  targetGlob: string
+  output: string
+  ignorePattern: string | RegExp | null
   dynamicPattern: string | RegExp
 }
 
 export const defaultConfig: Config = {
+  targetGlob: 'pages/**/*.tsx',
+  output: 'path.ts',
   ignorePattern: /^_.*/,
   dynamicPattern: /^\[(\w+)\]/,
 }
