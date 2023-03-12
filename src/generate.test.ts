68bdee3f56c272a5b647c78ad1abd4612cc0e110
Koichi Kiyokawa <koichi20110068@gmail.com>
2023-03-12 18:28:17 +0900

    test: update snapshot

--- src/generate.test.ts
+++ src/generate.test.ts
@@ -18,32 +18,32 @@ test("bracket", async () => {
     // This file is auto generated. DO NOT EDIT
 
     type PathToParams = {
-
+      
     }
 
     /**
-    * @example
-    * buildPath('/posts/[id]', { id: 1 }) // => '/posts/1'
-    */
+     * @example
+     * buildPath('/posts/[id]', { id: 1 }) // => '/posts/1'
+     */
     export function buildPath<Path extends keyof PathToParams>(
     path: Path,
     args: PathToParams[Path],
     ): string {
-    return (
-      path.replace(/\\\\[(\\\\w+)\\\\]/g, (_, key) => ((args as any).params)[key]) +
-      (args.query
-        ? '?' + new URLSearchParams(args.query as any).toString()
-        : '') +
-      (args.hash ? '#' + args.hash : '')
-    )
+      return (
+        path.replace(/\\\\[(\\\\w+)\\\\]/g, (_, key) => ((args as any).params)[key]) +
+        (args.query
+          ? '?' + new URLSearchParams(args.query as any).toString()
+          : '') +
+        (args.hash ? '#' + args.hash : '')
+      )
     }
 
     /**
-    * @example
-    * echoPath('/posts/[id]') // => '/posts/[id]'
-    */
+     * @example
+     * echoPath('/posts/[id]') // => '/posts/[id]'
+     */
     export function echoPath<Path extends keyof PathToParams>(path: Path): string {
-    return path
+      return path
     }
     "
   `);
@@ -57,32 +57,32 @@ it("colon", async () => {
     // This file is auto generated. DO NOT EDIT
 
     type PathToParams = {
-
+      
     }
 
     /**
-    * @example
-    * buildPath('/posts/[id]', { id: 1 }) // => '/posts/1'
-    */
+     * @example
+     * buildPath('/posts/[id]', { id: 1 }) // => '/posts/1'
+     */
     export function buildPath<Path extends keyof PathToParams>(
     path: Path,
     args: PathToParams[Path],
     ): string {
-    return (
-      path.replace(/\\\\[(\\\\w+)\\\\]/g, (_, key) => ((args as any).params)[key]) +
-      (args.query
-        ? '?' + new URLSearchParams(args.query as any).toString()
-        : '') +
-      (args.hash ? '#' + args.hash : '')
-    )
+      return (
+        path.replace(/\\\\[(\\\\w+)\\\\]/g, (_, key) => ((args as any).params)[key]) +
+        (args.query
+          ? '?' + new URLSearchParams(args.query as any).toString()
+          : '') +
+        (args.hash ? '#' + args.hash : '')
+      )
     }
 
     /**
-    * @example
-    * echoPath('/posts/[id]') // => '/posts/[id]'
-    */
+     * @example
+     * echoPath('/posts/[id]') // => '/posts/[id]'
+     */
     export function echoPath<Path extends keyof PathToParams>(path: Path): string {
-    return path
+      return path
     }
     "
   `);
